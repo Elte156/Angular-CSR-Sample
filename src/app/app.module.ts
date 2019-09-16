f31cbef0e284e566635783de62b0c74cb397e222
Pagaduan, Tony <TonyPagaduan@quickenloans.com>
2019-09-15 22:24:07 -0400

    Added pages for index, about, and news

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,26 @@ import { FormsModule }          from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { CrisisListComponent } from './crisis-list/crisis-list.component';
-import { HeroListComponent } from './hero-list/hero-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PageIndexComponent } from './page-index/page-index.component';
+import { PageAboutComponent } from './page-about/page-about.component';
+import { PageNewsComponent } from './page-news/page-news.component';
 
 const appRoutes: Routes = [
-  { path: 'crisis-center', component: CrisisListComponent },
-  { path: 'heroes', component: HeroListComponent },
-  { path: '',   redirectTo: '/heroes', pathMatch: 'full' },
+  { path: 'index', component: PageIndexComponent },
+  { path: 'about', component: PageAboutComponent },
+  { path: 'news', component: PageNewsComponent },
+  { path: '',   redirectTo: '/index', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    CrisisListComponent,
-    HeroListComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    PageIndexComponent,
+    PageAboutComponent,
+    PageNewsComponent
   ],
   imports: [
     BrowserModule,
