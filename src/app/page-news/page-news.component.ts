8fd87afe2f6dab27c32cf8abf77487d32d11dcff
Pagaduan, Tony <TonyPagaduan@quickenloans.com>
2019-09-16 21:37:24 -0400

    Added sample data for news * ngFor loop

--- src/app/page-news/page-news.component.ts
+++ src/app/page-news/page-news.component.ts
@@ -10,6 +10,26 @@ export class PageNewsComponent implements OnInit {
 
   private config: Config;
 
+
+  array = [
+    {
+      guid: '900ea552-ef68-42cc-b6a6-b8c4dff10fb7',
+      publishedAt: 32,
+      title: 'Powers Schneider',
+    },
+    {
+      guid: '880381d3-8dca-4aed-b207-b3b4e575a15f',
+      publishedAt: 25,
+      title: 'Adrian Lawrence',
+    },
+    {
+      guid: '87b47684-c465-4c51-8c88-3f1a1aa2671b',
+      publishedAt: 32,
+      title: 'Boyer Stanley',
+    },
+  ]
+
+
   constructor(private newsService: NewsService) { }
 
   ngOnInit() {
